587a5ff1839bb752d14e26e72ef1b28b3dbf3cb4
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2023-12-05 21:58:43 +0200

    init

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,9 +1,32 @@
 document.querySelector('.form__button').onclick = () => {
   const form = document.getElementById('form')
 
-  if (form.reportValidity()) {
-    form.submit()
-  } else {
-    form.reset()
+  //   const username = document.getElementById('username')
+  const email = document.getElementById('email')
+
+  //   if (form.reportValidity()) {
+  //     form.submit()
+  //   } else {
+  //     form.reset()
+  //   }
+
+  //   Перевірка на заповнення інпута (username)
+  //   if (username.checkValidity()) {
+  //     form.submit()
+  //   }
+
+  // Перевірка на заповнення інпута (email) з кастомним повідомленням про помилку
+
+  if (!email.checkValidity()) {
+    if (email.value.lenght === 0) {
+      email.setCustomValiditi('Помилка. Введіть значення')
+    }
+
+    if (email.value.lenght > 10) {
+      email.setCustomValiditi(
+        'Помилка. Введіть меньше 10  символів',
+      )
+    }
+    email.reportValidity()
   }
 }
