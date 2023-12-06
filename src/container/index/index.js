8a8f5ba89231fff25dbb21ca50c5c1fc48f5a5b3
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2023-12-06 13:04:57 +0200

    form complit

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,14 +1,13 @@
 document.querySelector('.form__button').onclick = () => {
   const form = document.getElementById('form')
-
-  //   const username = document.getElementById('username')
   const email = document.getElementById('email')
+  //   const username = document.getElementById('username')
 
-  //   if (form.reportValidity()) {
-  //     form.submit()
-  //   } else {
-  //     form.reset()
-  //   }
+  if (form.reportValidity()) {
+    form.submit()
+  } else {
+    form.reset()
+  }
 
   //   Перевірка на заповнення інпута (username)
   //   if (username.checkValidity()) {
@@ -21,14 +20,42 @@ document.querySelector('.form__button').onclick = () => {
 
   if (!email.checkValidity()) {
     if (email.value.length === 0) {
-      email.setCustomValiditi('Помилка. Введіть значення')
+      email.setCustomValidity('Помилка. Введіть значення')
     }
-
     if (email.value.length > 10) {
-      email.setCustomValiditi(
-        'Помилка. Введіть меньше 10  символів',
+      email.setCustomValidity(
+        'Помилка. Введіть менше 10 символів',
       )
     }
     email.reportValidity()
   }
 }
+
+document
+  .querySelectorAll('.form__button-add')
+  .forEach((element) => {
+    element.onclick = () => {
+      if (element.getAttribute('operator') === '+') {
+        return age.stepUp(
+          Number(element.getAttribute('value')),
+        )
+      }
+
+      if (element.getAttribute('operator') === '-') {
+        return age.stepDown(
+          Number(element.getAttribute('value')),
+        )
+      }
+    }
+  })
+
+document.querySelector('.form__button-save').onclick =
+  () => {
+    const value = document.getElementById('username').value
+
+    if (value.length === 0) alert('Даних нема')
+
+    navigator.clipboard
+      .writeText(value)
+      .then(() => alert('Дані зберегли'))
+  }
